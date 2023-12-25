58d664ea8eb7245989527b8864a951d472409f62
PQMIT <PQM2112000@gmail.com>
2023-12-26 00:07:42 +0700

    Updates

---
+++ precache-manifest.0c864d7dc4e85f393ac6e6da95b6c7b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08ed03806f6b4d195be173b56b6d99d",
+    "url": "/multichoice-crm/index.html"
+  },
+  {
+    "revision": "7435dfd4fd5f239a1440",
+    "url": "/multichoice-crm/static/css/main.07d30364.chunk.css"
+  },
+  {
+    "revision": "4398c40227884dbfc862",
+    "url": "/multichoice-crm/static/js/2.f4ed4392.chunk.js"
+  },
+  {
+    "revision": "dc58d78c6419fa276b58935aa9692eda",
+    "url": "/multichoice-crm/static/js/2.f4ed4392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7435dfd4fd5f239a1440",
+    "url": "/multichoice-crm/static/js/main.54731ef7.chunk.js"
+  },
+  {
+    "revision": "051a3e4c97238d9ef3de",
+    "url": "/multichoice-crm/static/js/runtime-main.a533becc.js"
+  },
+  {
+    "revision": "0d7c728b9b94a93a44fc63e2835e0494",
+    "url": "/multichoice-crm/static/media/font_1.0d7c728b.woff"
+  }
+]);
