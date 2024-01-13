5c548860ef6d5ec1c379fbcda0c36aff969b67b8
PQMIT <PQM2112000@gmail.com>
2024-01-13 13:19:48 +0700

    Updates

---
+++ precache-manifest.7306dff4312071d18dcb0902a4e0dd6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ec80bd683a71cb8a15f8864dc84171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89da5c4fe538c1aeae70",
+    "url": "/static/css/main.4d0b6f7f.chunk.css"
+  },
+  {
+    "revision": "b2dfc5852f1343d188cf",
+    "url": "/static/js/2.8e07a419.chunk.js"
+  },
+  {
+    "revision": "d9f5b39aae58017a105c14a84c731000",
+    "url": "/static/js/2.8e07a419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89da5c4fe538c1aeae70",
+    "url": "/static/js/main.e67f060c.chunk.js"
+  },
+  {
+    "revision": "672b856247f80f3f23d8",
+    "url": "/static/js/runtime-main.503b11d8.js"
+  },
+  {
+    "revision": "0d7c728b9b94a93a44fc63e2835e0494",
+    "url": "/static/media/font_1.0d7c728b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8afbca03fb087233ec0d553765d49f5d.js"
+  "/precache-manifest.7306dff4312071d18dcb0902a4e0dd6e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
