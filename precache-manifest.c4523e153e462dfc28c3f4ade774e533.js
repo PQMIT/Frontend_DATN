ca0c4a395f1e29fac9e6ee63b769918475851917
PQMIT <PQM2112000@gmail.com>
2024-01-20 01:15:12 +0700

    Updates

---
+++ precache-manifest.c4523e153e462dfc28c3f4ade774e533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e336c945770784ab1e3741d33b29e4",
+    "url": "/pqmit.github.io/index.html"
+  },
+  {
+    "revision": "8dc425ba9d737120d94f",
+    "url": "/pqmit.github.io/static/css/main.8b1b41cc.chunk.css"
+  },
+  {
+    "revision": "9508d8193710221ecfde",
+    "url": "/pqmit.github.io/static/js/2.fbf7b7a6.chunk.js"
+  },
+  {
+    "revision": "2b53c335af1ced25563e230dc4d7be54",
+    "url": "/pqmit.github.io/static/js/2.fbf7b7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc425ba9d737120d94f",
+    "url": "/pqmit.github.io/static/js/main.8b70ff2a.chunk.js"
+  },
+  {
+    "revision": "6ef3f4a1aabdbf09ab56",
+    "url": "/pqmit.github.io/static/js/runtime-main.1ceb06ee.js"
+  },
+  {
+    "revision": "0d7c728b9b94a93a44fc63e2835e0494",
+    "url": "/pqmit.github.io/static/media/font_1.0d7c728b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a04f8de2b484189f9186105f0ea7cd06.js"
+  "/pqmit.github.io/precache-manifest.c4523e153e462dfc28c3f4ade774e533.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pqmit.github.io/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
